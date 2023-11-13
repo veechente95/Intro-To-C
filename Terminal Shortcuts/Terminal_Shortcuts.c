203a7129a56874e95a46c55375d6635ff4f2218f
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-11-13 13:50:57 -0800

    Update Terminal_Shortcuts.c

--- Terminal Shortcuts/Terminal_Shortcuts.c
+++ Terminal Shortcuts/Terminal_Shortcuts.c
@@ -1,1 +1,8 @@
 // List of useful terminal shortcuts
+
+// How to install software and run minicom on raspberry Pi
+sudo apt install minicom        // install minicom software on Raspberry Pi
+minicom -h                      // show minicom options 
+ls /dev/ttyUSB0                 // used to check if a USB-to-serial device (like UART) is detected by the system
+sudo minicom -D /dev/ttyUSB0    // creates a connection to the serial port located at /dev/ttyUSB0
+CTRL -A                         // help 
