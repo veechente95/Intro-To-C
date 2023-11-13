59073c69b436c2c09cb1b9a09c1f784f95a2fcd7
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-11-13 14:02:32 -0800

    Update Terminal_Shortcuts.c

--- Terminal Shortcuts/Terminal_Shortcuts.c
+++ Terminal Shortcuts/Terminal_Shortcuts.c
@@ -1,8 +1,21 @@
 // List of useful terminal shortcuts
 
-// How to install software and run minicom on raspberry Pi
+// How to install software and run minicom on raspberry pi
 sudo apt install minicom        // install minicom software on Raspberry Pi
 minicom -h                      // show minicom options 
 ls /dev/ttyUSB0                 // used to check if a USB-to-serial device (like UART) is detected by the system
 sudo minicom -D /dev/ttyUSB0    // creates a connection to the serial port located at /dev/ttyUSB0
 CTRL -A                         // help 
+
+// Install Wiring Pi Library
+sudo apt-get update            // update package
+git clone https://github.com/WiringPi/WiringPi.git  // clone wiringPi github directory
+cd WiringPi                    // navigate to the WiringPi Directory
+
+// How to configure raspberry pi
+sudo raspi-config              // system options --> Wireless LAN --> add network name and password 
+
+// Connect to Raspberry Pi Over SSH
+ssh pi@PiIpAddress            // connect to Raspberry Pi over SSH
+hostname -I                   // gives you IP Address
+
